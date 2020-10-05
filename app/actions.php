ba1725c79a8660d4bc792822df0b959c89adcd13
Praneeth Polu <contact@polupraneeth.me>
2020-10-05 20:33:59 +0530

    action file add with deragister action

--- app/actions.php
+++ app/actions.php
@@ -2,9 +2,26 @@
 
 namespace App;
 
-function website_favicon() { ?>
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php asset('images/apple-touch-icon.png') ?>" >
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php asset('images/favicon-32x32.png') ?>" >
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php asset('images/favicon-16x16.png') ?>" >
-<?php }
-add_action('wp_head', 'website_favicon');
+
+/**
+ * Add favicon to head
+ */
+add_action('wp_head', function () {
+    ?>
+    	<link rel="apple-touch-icon" sizes="180x180" href=" <?php asset_path('images/apple-touch-icon.png') ?> " >
+    	<link rel="icon" type="image/png" sizes="32x32" href=" <?php asset_path('images/favicon-32x32.png') ?> " >
+    	<link rel="icon" type="image/png" sizes="16x16" href=" <?php asset_path('images/favicon-16x16.png') ?> " >
+    <?php
+});
+
+/**
+ * Deragister styles and scripts
+ */
+add_action( 'wp_enqueue_scripts', function () {
+    	
+    	//scripts
+    	wp_dequeue_script( 'contact-form-7' );
+
+    	//styles
+        wp_dequeue_style( 'contact-form-7' );
+ });
