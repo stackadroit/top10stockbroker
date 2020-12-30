bf9fd95f6ede9e211fc6af37b2bf4d32e38aebc3
Pavan <dropmail2pavan@gmail.com>
2020-12-30 19:48:01 +0530

    Change calculates page text.

--- resources/views/shortcodes/calculator/tax_equivlent_yield_calculator.blade.php
+++ resources/views/shortcodes/calculator/tax_equivlent_yield_calculator.blade.php
@@ -2,20 +2,20 @@
 <section class="couter shotcodwraper">       
     <div class="cinner form-row shortcode-bg">
         <div class="col-md-6 mt-1 mt-sm-2">
-            <span>Tax Free Yield*</span>
+            <span>Tax Free Yield (%) *</span>
         </div>
         <div class="col-md-6 mt-1 mt-sm-2">
             <input type="number" class="form-control" value="5" id="tfy_id">
         </div>
         <div class="col-md-6 mt-1 mt-sm-2">
-            <span>Tax Rate </span>
+            <span>Tax Rate (%)</span>
         </div>
         <div class="col-md-6 mt-1 mt-sm-2">
             <input type="number" class="form-control" value="30" id="tr1_id" >
         </div>
         <hr class="hori col-md-12">
         <div class="col-md-6 mt-1 mt-sm-2">
-            <span class="pad-rt">Tax Equivalent Yield </span>
+            <span class="pad-rt">Tax Equivalent Yield (%)</span>
         </div>
         <div class="col-md-6 mt-1 mt-sm-2"> 
             <input  class="form-control"   type="text" value="7.1" id="tey2_id" readonly>
