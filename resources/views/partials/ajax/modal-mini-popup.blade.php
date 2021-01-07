ef273491a6f6274e3954c7ee32a256843467e743
Ashi05 <44727979+Ashi05@users.noreply.github.com>
2021-01-07 23:10:27 +0530

    pop up changes

--- resources/views/partials/ajax/modal-mini-popup.blade.php
+++ resources/views/partials/ajax/modal-mini-popup.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid py-4">
   <h4  style="text-align: center;">{{$head_title}}</h4>
   {!! do_shortcode($do_contactform) !!}
-  <p class="mini"  style="text-align: center;">
+  <p class="mini mb-0"  style="text-align: center;">
     {{$btm_content}}
   </p>
 </div>
